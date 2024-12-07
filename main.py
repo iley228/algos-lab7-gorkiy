c4cf00a4afcccd64d57941ea7fcff4d2854acbf0
iley228 <gorkijila@gmail.com>
2024-12-07 09:18:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,45 @@
 
 Пример использования isPalindromString("Madam, Anna went to the civic center") # ["madam", "anna", "civic"]
 """
+def isPalindrom(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
+    
+user_input = input('Введите слово: ')
+
+print(isPalindrom(user_input))
+
+def isPalindromList(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
+
+def check_palindromes(words):
+    palindromes = []
+    for word in words:
+        if isPalindromList(word):
+            palindromes.append(word.lower())
+    return palindromes
+
+user_input = input("Введите слова через запятую: ")
+words_list = [word.strip() for word in user_input.split(",")]
+
+print(check_palindromes(words_list))
+
+def isPalindromString(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
+
+def find_palindromes(text):
+    words = text.split()
+    palindromes = set()
+    
+    for word in words:
+        word = ''.join(char for char in word if char.isalnum())
+        if isPalindromString(word) and word:
+            palindromes.add(word.lower())
+
+    return list(palindromes)
+
+user_input = input("Введите текст: ")
+
+print(find_palindromes(user_input))
